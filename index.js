cf831afd16a03cd1518b0e37f9820b46a93fddea
nusu <nusususuzu@gmail.com>
2016-07-01 14:10:02 +0300

    kuzgun file implementation, kuzgun brak method

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ var kuzgun      = require('commander'),
     fs          = require("fs"),
     path        = require("path"),
     r           = require("inquirer"),
-    SSH         = require('simple-ssh'),
-    ncp         = require("copy-paste");
+    Client      = require('ssh2').Client,
+    ncp         = require("copy-paste"),
+    chalk       = require("chalk");
 
 // prompt
 kuzgun
@@ -16,9 +17,9 @@ kuzgun
     .option('-i, --init <init>', 'create kuzgun file')
     .option('-t, --test <test>', 'testet')
     .option('-B, --birth <birth>', 'create kuzgun file mystically')
+    .option('-b, --brak <brak>', 'send kuzgun for pulling repository')
     .option('-f, --flight <flight>', 'send kuzgun for observation flight')
-    .option('-r, --reborn <reborn>', 'config for raven system')
-    .option('-k, --raven <raven>', 'config for raven system')
+    .option('-k, --raven <raven>', 'config for kuzgun system')
     .option('-c, --capture <capture>', 'alias of ssh with information, capture your ravens body and flight')
     .parse(process.argv);
 
@@ -52,41 +53,60 @@ if( !ravenWhereareYou.contains(".kuzgun")){
     }
 }
 var ravenFile = JSON.parse(fs.readFileSync('.kuzgun', 'utf8'));
-var  privateKey = ravenFile.sshprivate;
-//console.log(JSON.stringify(ravenFile, null, '   '));
-var ssh = new SSH({
-    host: ravenFile.server,
-    user: ravenFile.suser,
-    //pass: ravenFile.spassword
-    key: fs.readFileSync(privateKey)
-});
-
+var privateKey = ravenFile.sshprivate;
 if(kuzgun.test){
-    ssh
-        .exec("touch something.happening",{
-            out: console.log.bind(console)
-        })
-        .start();
-    ssh.on('error', function(err) {
-        console.log('Oops, something went wrong.');
-        console.log(err);
-        ssh.end();
-    });
+
 }
 
 if(kuzgun.flight){
     // for add ssh key to knowing hosts
-    ssh
-        .exec("mkdir ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys; chmod 700 ~/.ssh",{
-            out: console.log.bind(console)
-        }).exec("printf '%s\n' '"+ fs.readFileSync(ravenFile.sshpublic) +"' > ~/.ssh/authorized_keys", {
-            out: console.log.bind(console)
-        })
-        .start();
-    ssh.on('error', function(err) {
-        console.log('Oops, something went wrong.');
-        console.log(err);
-        ssh.end();
+    var flightClient = new Client();
+    flightClient.on('ready', function() {
+        console.log(chalk.magenta('Kuzgun has been arrived the destination'));
+        flightClient.exec('mkdir ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys; chmod 700 ~/.ssh', function(err, stream) {
+            console.log(chalk.magenta("Kuzgun trying to create ~/.ssh/authorized_keys directory"));
+            if (err) {
+                console.log(chalk.red('Kuzgun has been fall while creating directory, It can be a permission error, log: ' + err));
+                return flightClient.end();
+            }
+            stream.on('end', function() {
+                console.log(chalk.yellow("Kuzgun created the directory"));
+                return flightClient.end();
+            }).on('data', function(data) {
+                console.log(data.toString());
+            });
+        });
+        flightClient.exec("printf '%s\n' '"+ fs.readFileSync(ravenFile.sshpublic) +"' > ~/.ssh/authorized_keys", function(err, stream) {
+            console.log(chalk.magenta("Kuzgun trying to copy your key to authorized_keys"));
+            if (err) {
+                console.log(chalk.red('Kuzgun has been fall while copying your public key to authorized keys, log: ' + err));
+                return flightClient.end();
+            }
+            stream.on('end', function() {
+                console.log(chalk.yellow("Kuzgun successfully pasted your key"));
+                return flightClient.end();
+            }).on('data', function(data) {
+                console.log(data.toString());
+            });
+        });
+        flightClient.exec("cd "+ravenFile.dir+"; git clone "+ravenFile.repository, function(err, stream) {
+            console.log(chalk.magenta("Kuzgun trying to clone your repository"));
+            if (err) {
+                console.log(chalk.red('Kuzgun has been fall while pulling your repository, log: ' + err));
+                return flightClient.end();
+            }
+            stream.on('end', function() {
+                console.log(chalk.yellow("Kuzgun successfully cloned your repository"));
+                return flightClient.end();
+            }).on('data', function(data) {
+                console.log(data.toString());
+            });
+        });
+    }).connect({
+        host: ravenFile.server,
+        username: ravenFile.suser,
+        password: ravenFile.spassword,
+        port: 22
     });
 }
 
@@ -94,17 +114,44 @@ if(kuzgun.capture) {
     var sshInfo = 'ssh ' + ravenFile.suser + '@' + ravenFile.server,
         isWin = /^win/.test(process.platform);
     if( isWin ){
-        console.log("This feature is unnecessary in windows because windows terminal emulator doesn't have ssh support.");
+        console.log(chalk.red("This feature is unnecessary in windows because windows terminal emulator doesn't have ssh support."));
     }
     ncp.copy(sshInfo, function () {
-        console.log("ssh predefined login informations has been copied to your clipboard.");
-        console.log("have a good flight");
+        console.log(chalk.magenta("ssh predefined login informations has been copied to your clipboard."));
+        console.log(chalk.magenta("have a good flight"));
     });
 }
 
-
 if(kuzgun.brak){
-
+    var brakClient = new Client();
+    var kuzgunPath = ravenFile.dir,
+        kuzgunBranch= ravenFile.alias+" "+ravenFile.branch;
+    brakClient.on('ready', function() {
+        brakClient.exec('cd '+kuzgunPath+'; git pull '+kuzgunBranch, function(err, stream) {
+            if (err) {
+                console.log(chalk.red('Kuzgun has been fall while pulling the repository, log: ' + err));
+                return brakClient.end();
+            }
+            stream.on('end', function() {
+                return brakClient.end();
+            }).on('data', function(data) {
+                console.log(data.toString());
+            });
+            stream.on('close', function(code, signal) {
+                console.log(chalk.magenta('Stream :: close :: code: ' + code + ', signal: ' + signal));
+                brakClient.end();
+            }).on('data', function(data) {
+                console.log(chalk.magenta('Server Said (STDOUT): ' + data));
+            }).stderr.on('data', function(data) {
+                console.log(chalk.magenta('Server Said (STDERR): ' + data));
+            });
+        });
+    }).connect({
+        host: ravenFile.server,
+        username: ravenFile.suser,
+        privateKey: fs.readFileSync(privateKey),
+        port: 22
+    });
 }
 
 if(kuzgun.init || kuzgun.birth){
@@ -167,6 +214,25 @@ if(kuzgun.init || kuzgun.birth){
             name: "sshpublic",
             message: "DSA type ssh Public Key",
             choices: sshPub
+        },
+        {
+            name: "repository",
+            message: "repository url",
+            default: ""
+        },
+        {
+            name:"alias",
+            message: "repository remote alias",
+            default: "origin"
+        },
+        {
+            name:"branch",
+            message: "branch name that kuzgun going to pull?",
+            default: "master"
+        },
+        {
+            name:"dir",
+            message: "working directory:"
         }
     ];
     var ravenJson = {};
@@ -177,15 +243,20 @@ if(kuzgun.init || kuzgun.birth){
             spassword: answers.spassword,
             sport: answers.sport,
             sshprivate: sshExactPath+'/'+answers.sshprivate,
-            sshpublic: sshExactPath+'/'+answers.sshpublic
+            sshpublic: sshExactPath+'/'+answers.sshpublic,
+            alias: answers.alias,
+            branch: answers.branch,
+            dir: answers.dir,
+            repository: answers.repository
         };
         console.log(JSON.stringify(ravenJson, null, '   '));
         var outputFilename = ".kuzgun";
         fs.writeFile(outputFilename, JSON.stringify(ravenJson, null, 4), function(err) {
          if(err) {
-            console.log("kuzgun suddenly fall:" + err);
+            console.log(chalk.red("kuzgun suddenly fall:" + err));
          } else {
-            console.log("kuzgun is ready to serve");
+            console.log(chalk.green("kuzgun is ready to serve"));
+            console.log(chalk.yellow("don't forget to add .kuzgun to .gitignore"));
          }
         });
     });
